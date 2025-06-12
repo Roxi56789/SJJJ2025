94ab3bee1ca22df40bc479c4454f856c27d5ad7c
Jazmin Aguilar <Jazmin086441@outlook.es>
2025-06-11 22:43:05 -0600

    se realizo el formulario de usuarios

--- src/main/java/Esfe/presentacion/UserReadingForm.java
+++ src/main/java/Esfe/presentacion/UserReadingForm.java
@@ -3,4 +3,10 @@ package Esfe.presentacion;
 import javax.swing.*;
 
 public class UserReadingForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JButton button1;
+    private JTable table1;
+    private JButton button2;
+    private JButton button3;
 }
