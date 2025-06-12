e61bd10fafa3a2699899d5d1bd8de80180166eab
Roxana Jeannette Caballero Hernandez <rh0079937@gmail.com>
2025-06-11 20:47:43 -0600

    iDDsiseño del formulario de cambiar contraseña

--- src/main/java/Esfe/presentacion/ChangePasswordForm.java
+++ src/main/java/Esfe/presentacion/ChangePasswordForm.java
@@ -3,4 +3,8 @@ package Esfe.presentacion;
 import javax.swing.*;
 
 public class ChangePasswordForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField textEmail;
+    private JTextField txtPassword;
+    private JButton btnChangePassword;
 }
